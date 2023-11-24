b710bcfd46c849ad4da896faa1b397aa3d9b8dd7
kelvtmoney <kelvtmoni@gmail.com>
2023-11-24 11:21:08 +0100

    added more function

--- template-infinity-scroll/script.js
+++ template-infinity-scroll/script.js
@@ -2,7 +2,25 @@
 const imgContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+//unsplash API
+const count = 30;
+const apiKey = "TwXFVphGeu43IxzjEy3WGDbe5WiPrIFEGUegP0jPx6Y";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all image were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log("ready =", ready);
+  }
+}
 
 // helper function to set attribute
 function settingAttribute(element, arrAtribute) {
@@ -12,6 +30,9 @@ function settingAttribute(element, arrAtribute) {
 }
 
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images=", totalImages);
   photosArray.forEach((photo) => {
     // create unplash link
     const item = document.createElement("a");
@@ -30,6 +51,10 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+
+    // check to see if image is loaded and execute a function
+    img.addEventListener("load", imageLoaded);
+
     // img.setAttribute("title", photo.alt_description);
     // appending element
     item.appendChild(img);
@@ -37,22 +62,35 @@ function displayPhotos() {
   });
 }
 
-//unsplash API
-const count = 10;
-const apiKey = "TwXFVphGeu43IxzjEy3WGDbe5WiPrIFEGUegP0jPx6Y";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 // get photo from unsplah API
 async function getPhoto() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
+    // console.log(photosArray);
     displayPhotos();
   } catch (error) {
     console.log(error);
     //catch erro here
   }
 }
+
+// cheeck to see if scrolling near buttom,load more image
+window.addEventListener("scroll", function (e) {
+  if (
+    this.window.innerHeight + this.window.scrollY >=
+      this.document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    // console.log(this.window.innerHeight);
+    // console.log(this.window.scrollY);
+    // console.log(this.document.body.offsetHeight - 1000);
+    getPhoto();
+    console.log("scrolled");
+  }
+});
+
 // on load
 getPhoto();
 // console.dir(imgContainer);
